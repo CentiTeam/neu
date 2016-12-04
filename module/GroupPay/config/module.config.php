adaf96a7c1c21b49f7a39ed337df5cfbc8be8c38
Aron <Aron@aronzwei>
2016-12-04 15:47:29 +0100

    test

--- module/GroupPay/config/module.config.php
+++ module/GroupPay/config/module.config.php
@@ -32,6 +32,11 @@ return [
     ],
 		
 		'view_manager' => [
+				'display_not_found_reason' => true,
+				'display_exceptions'       => true,
+				'doctype'                  => 'HTML5',
+				'not_found_template'       => 'error/404',
+				'exception_template'       => 'error/index',
 				'template_map' => [
 						'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
 						'groupPay/groupPay/index' => __DIR__ . '/../view/groupPay/groupPay/index.phtml',
