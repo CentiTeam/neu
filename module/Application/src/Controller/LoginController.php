e265825af7afcf88ff6109aef7c59329ea5476d4
Alex <Alex@Alex-PC>
2016-12-07 10:39:22 +0100

    kkkk

--- module/Application/src/Controller/LoginController.php
+++ module/Application/src/Controller/LoginController.php
@@ -6,8 +6,10 @@ use Zend\View\Model\ViewModel;
 
 class loginController extends AbstractActionController{
 	
-public function loginAction(){
+	public function loginAction(){
+		
+		echo $_POST('uname');
 	
-	return new ViewModel();
-}
+		return new ViewModel();
+	}
 }
