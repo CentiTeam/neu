e20ab7b61612408fb2a28872b732dd401584cc0c
Alex <Alex@Alex-PC>
2016-12-07 09:44:41 +0100

    login controller

---
+++ module/Application/src/Controller/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class loginController extends AbstractActionController{
+	
+public function loginAction(){
+	
+	return new ViewModel();
+}
+}
