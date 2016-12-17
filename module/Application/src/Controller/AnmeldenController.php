70a26506676f732effed324a1aff1bf0f7856aa7
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-17 17:48:11 +0100

    dasfd

--- module/Application/src/Controller/AnmeldenController.php
+++ module/Application/src/Controller/AnmeldenController.php
@@ -4,7 +4,7 @@
 * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
 * @license   http://framework.zend.com/license/new-bsd New BSD License
 */
-/**
+
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
@@ -18,4 +18,3 @@ class AnmeldenController extends AbstractActionController
 		return new ViewModel();
 	}
 }
-*/
