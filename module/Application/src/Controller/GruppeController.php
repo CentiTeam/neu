718af0ad1b6c82f2338a19e6d68b3a8b42e2a783
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-17 15:06:50 +0100

    Gruppe anlegen inkl. MVC

---
+++ module/Application/src/Controller/GruppeController.php
@@ -0,0 +1,48 @@
+<?php
+namespace Application\Controller;
+ 
+ 
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+ 
+ 
+/**
+ *
+ */
+class GruppeController extends AbstractActionController {
+     
+    function anlegenAction() {
+        // TODO Berechtigungsprüfung
+                 
+        $gruppe = new Gruppe();       
+         
+        if ($_REQUEST['speichern']) {
+            // Array, das eventuelle Fehlermeldungen enthält
+            
+        	$saved= false;
+        	
+        	$errors = array();
+             
+            // Pruefung-Objekt mit Daten aus Request-Array füllen
+            $gruppe->setGruppenname($_REQUEST["gruppenname"]);
+            $gruppe->setGruppenbeschreibung($_REQUEST["gruppenbeschreibung"]);
+            $gruppe->setGruppenbildpfad($_REQUEST["gruppenbildpfad"]);
+            
+            // Funktion der Klasse 
+            $isOk = $gruppe->anlegen();
+            
+            if (!$isOk) {
+            	array_push($errors, "Fehler beim anlegen der Gruppe!");
+            } else {
+            	$saved = true;
+            }
+            	
+        } 
+         
+         
+        return new ViewModel([
+                'gruppe' => array($gruppe),
+                'errors'   => $errors
+        ]);
+    }
+}
--- module/Application/src/Model/Gruppe.class.php
+++ module/Application/src/Model/Gruppe.class.php
@@ -10,15 +10,27 @@ class gruppe {
 	private $gruppenbeschreibung;
 	private $gruppenbildpfad;
 	
-	public function _construct($gruppen_id) {
-		$this->gruppenname;
-		$this->gruppenbeschreibung;
-		$this->gruppenbildpfad;
-		
-		$this->g_id=$gruppen_id;
+	public function __construct() {
+		$this->gruppenname = "";
+		$this->gruppenbeschreibung = "";
+		$this->gruppenbildpfad ="";
 	}
 	
 	public function anlegen () {
+		
+		$db = new DB_connection();
+		
+		$query = "INSERT INTO gruppe (gruppenname, gruppenbeschreibung, gruppenbildpfad) VALUES (
+				'".$this->gruppenname."', 
+				'".$this->gruppenbeschreibung."',
+				'".$this->gruppenbildpfad."')" ;
+
+		$result = $db->execute($query);
+		
+		// GENAUE SYNTAX FEHLT!!
+		$isOK = mysqli_affected_rows ($result) > 0;
+		
+		return $isOK;
 		
 	}
 	
@@ -29,4 +41,44 @@ class gruppe {
 	public function listeHolen() {
 		
 	}
+	
+	
+	
+	
+	// Getter und Setter
+	
+	public function getG_id () {
+		return $this->g_id;
+	}
+	
+	public function setG_Id() {
+		$this->g_id= $g_id;
+	}
+	
+	public function getGruppenname () {
+		return $this->gruppenname;
+	}
+	
+	public function setGruppenname() {
+		$this->gruppenname= $gruppenname;
+	}
+	
+	public function getGruppenbeschreibung () {
+		return $this->gruppenbeschreibung;
+	}
+	
+	public function setGruppenbeschreibung() {
+		$this->gruppenbeschreibung= $gruppenbeschreibung;
+	}
+	
+	public function getGruppenbildpfad () {
+		return $this->gruppenbildpfad;
+	}
+	
+	public function setGruppenbildpfad() {
+		$this->gruppenbildpfad= $gruppenbildpfad;
+	}
+	
+	
+	
 }
