b4ee30bc724a6d0281b29fce913df50a5fba021a
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-19 18:13:51 +0100

    FEWF

--- module/Application/src/Controller/OverviewController.php
+++ module/Application/src/Controller/OverviewController.php
@@ -9,7 +9,7 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
+use Application\Model\User;
 
 class overviewController extends AbstractActionController
 {
