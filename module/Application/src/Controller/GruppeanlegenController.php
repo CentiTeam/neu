d65d62eae3edb9611ae4d5356403edbe0c0df54a
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-19 18:48:13 +0100

    FEDF

--- module/Application/src/Controller/GruppeanlegenController.php
+++ module/Application/src/Controller/GruppeanlegenController.php
@@ -18,7 +18,7 @@ class GruppeanlegenController extends AbstractActionController {
 		$errors = array(); 
 		
 		// if($_SESSION['angemeldet'] != 'ja') {
-		if(!$user->isloggedin()) {
+		if(!$user[0]->isloggedin()) {
 			
 			array_push($errors, "Sie müssen angemeldet sein um eine Gruppe zu erstellen!");
 			
