4ac88aa8bc2b680f06c080aef68fb427c6c47076
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-19 15:47:10 +0100

    Userinfos speicherrn bei Login

--- module/Application/src/Controller/LoginController.php
+++ module/Application/src/Controller/LoginController.php
@@ -36,26 +36,39 @@ class loginController extends AbstractActionController{
 			//Wenn Werte aus der Datenbank in $result geschrieben wurden, dann wird weitergemacht
 			else{
 					//Holen der ersten (und hier einzigen, da nur ein Benutzername) Zeile des Ergebnisses
+					// Mit derr Funktion mysqli_fetch_array($result) anstelle von mysqli_fetch_row($result) können danach die Parameter 
+					// anhand dem Spaltennamen ausgelesen werden
+					
+					
+					
 					$row=mysqli_fetch_array($result);
 			
 					//Prï¿½fen, ob das eingegebene Passwort korrekt ist und der Benutzer aktiviert ist
 				
-					if($row['passwort'] == $pwd && $row[6]==0){
+					if($row['passwort'] == $pwd && $row['deaktiviert']==0){
 					echo $row['passwort'];
 					//Wenn man angemeldet ist, so wird dies in der Sessionvariable "angemeldet" gespeichert.
 					$_SESSION['angemeldet'] = "ja";
 					
 					// User laden aus Datenbank
-					//$u_id=$row[0];
+					$user= new User();
 					
-					
+					$user->u_id=$row['u_id'];
+					$user->username=$row['username'];
+					$user->vorname=$row['vorname'];
+					$user->nachname=$row['nachname'];
+					$user->passwort=$row['passwort'];
+					$user->email=$row['email'];
+					$user->deaktiviert=$row['deaktiviert'];
+					$user->systemadmin=$row['systemadmin'];
 					
 					
 					
 					
 					$view = new ViewModel(array(
 							'message' => 'Erfolgreich eingeloggt!',
-							'uname' => $uname
+							'uname' => $uname,
+							'user' => array($user)
 					));
 					$view->setTemplate('application/overview/overview.phtml');
 					return $view;
