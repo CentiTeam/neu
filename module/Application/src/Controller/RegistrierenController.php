b7c58699376aa1d8cb700cd70f319025e971cb9d
Mattaaal <buchbauer.matthias@yahoo.de>
2016-12-20 14:27:07 +0100

    nochmal

--- module/Application/src/Controller/RegistrierenController.php
+++ module/Application/src/Controller/RegistrierenController.php
@@ -4,15 +4,14 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Application\Model\DB_connection;
-use Application\Model\User;
-// require_once('Application/src/model/User.php');
 
 
 class registrierenController extends AbstractActionController{
 
 	public function registrierenAction(){
-
+		
+		return new ViewModel();
+		
 		
 	}
 }
