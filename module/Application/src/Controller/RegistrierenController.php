d8200909aa5e7fd39b568b31470c10b0cfdfda57
Mattaaal <buchbauer.matthias@yahoo.de>
2016-12-20 14:25:42 +0100

    Teil des Registrierungscontrollers

---
+++ module/Application/src/Controller/RegistrierenController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Application\Model\DB_connection;
+use Application\Model\User;
+// require_once('Application/src/model/User.php');
+
+
+class registrierenController extends AbstractActionController{
+
+	public function registrierenAction(){
+
+		
+	}
+}
