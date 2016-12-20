35c5d93da0979e8528b6e2a378a1bf52704d5042
FENN01 <FENN01@ZIM4743.rz.uni-passau.de>
2016-12-20 14:30:19 +0100

    edwfe

--- module/Application/src/Controller/GruppeanlegenController.php
+++ module/Application/src/Controller/GruppeanlegenController.php
@@ -47,7 +47,7 @@ class GruppeanlegenController extends AbstractActionController {
 				
 			if ($gruppenname=="Kinderporno") {
 				$errorStr .="Der Gruppenname darf nicht Kinderporno heißen!<br>";
-				die ("KInderpornotest");
+				die ("KInderpornotest1");
 			}
 				
 			var_dump($gruppenname);
