f1a535e4ccef10c231ee03d9333a967da81560c1
Alex <Alex@Alex-PC>
2016-12-22 12:19:34 +0100

    Doppeltes ==

--- module/Application/src/Model/User.php
+++ module/Application/src/Model/User.php
@@ -99,13 +99,12 @@ class User
 				$this->email = $row['email'];
 				$this->deaktiviert = $row['deaktiviert'];
 				$this->systemadmin = $row['systemadmin']; 
-				
-				echo $row['systemadmin']; 
+
 
 				//Wenn es sich um einen Systemadmin handelt, dann wird die Sessionvariable "angemeldet", welche für angemeldeter Benutzer steht, mit nein überschrieben und die Sessionvariable "systemadmin" erhält den Wert ja
 				if($row['systemadmin'] == 1){
-					$_SESSION['systemadmin'] == "ja";
-					$_SESSION['angemeldet'] == "nein";
+					$_SESSION['systemadmin'] = "ja";
+					$_SESSION['angemeldet'] = "nein";
 					//echo "test";
 				}
 				
