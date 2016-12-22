05d193fe6faaa5b0dcb9e05673627dd718fcbf97
Alex <Alex@Alex-PC>
2016-12-22 11:37:50 +0100

    j

--- module/Application/src/Model/User.php
+++ module/Application/src/Model/User.php
@@ -30,9 +30,9 @@ class User
 			//Erstellen und Ausführen einer Query zum Überprüfen, ob die eingegebene E-Mailadresse bereits verwendet wird
 			$query_emailueberpruefung = "SELECT email FROM User WHERE email ='".$email."';";
 			$result_emailueberpruefung = $db->execute($query_emailueberpruefung);
-			//echo $result_emailueberpruefung;
+			echo $result_emailueberpruefung;
 			//echo mysqli_num_rows($result_emailueberpruefung);
-			echo "test";
+
 			
 			//Falls E-Mailadresse noch nicht verwendet wird, dann Schreiben der Daten in die Datenbank.
 			if(mysqli_num_rows($result_emailueberpruefung) == 0){ 
