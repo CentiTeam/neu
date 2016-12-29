754aaec20bbfee676409244d3f2ca19cc421da33
Tanja <Tanja@CoolMaster.fritz.box>
2016-12-29 17:38:03 +0100

    dsgr

--- module/Application/src/Model/Gruppe.php
+++ module/Application/src/Model/Gruppe.php
@@ -69,9 +69,11 @@ class Gruppe {
 				
 			// neues Model ans Ende des $gruppeListe-Arrays anf�gen
 			$gruppeListe[] = $model;
+			
+
+			var_dump($gruppeListe->getGruppenname());
 		}
 		
-		var_dump($gruppeListe->getGruppenname());
 		// fertige Liste von Gruppe-Objekten zur�ckgeben
 		return $gruppeListe;
 		}
