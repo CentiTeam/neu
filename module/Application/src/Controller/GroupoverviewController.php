42298d4305024ce6a21678318cc831f7d79d4ff3
Tanja <Tanja@CoolMaster.fritz.box>
2016-12-29 16:01:57 +0100

    dsa

--- module/Application/src/Controller/GroupoverviewController.php
+++ module/Application/src/Controller/GroupoverviewController.php
@@ -4,6 +4,7 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Model\Gruppe;
 
 class GroupoverviewController extends AbstractActionController
 {
