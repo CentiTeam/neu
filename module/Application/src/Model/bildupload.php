19719cace8fd6fe301678a5305e28d1d222630d0
Veri <Veri@Verena>
2016-12-31 14:34:59 +0100

    Funktion für Bildupload als Klasse ausgelagert

---
+++ module/Application/src/Model/bildupload.php
@@ -0,0 +1,55 @@
+<?php
+namespace Application\Model;
+
+class bildupload
+{
+	private $upload_folder    			= "/img/";	
+	private $allowed_extensions			= array("png", "jpg", "jpeg", "gif");
+	private $max_size					= 2048*2048;
+	private $allowed_types				= array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+
+
+	public function uploadbild($uploadedfile) {
+
+		//Pfadangabe	
+		$filename = pathinfo($_FILES["uploadedfile"]["name"], PATHINFO_FILENAME);
+		$extension = strtolower(pathinfo($_FILES["uploadedfile"]["name"], PATHINFO_EXTENSION));
+		
+		//Dateiendung überprüfen
+		if(!in_array($extension, $this->allowed_extensions))
+		{
+			die ("Ungültige Dateiendung! Nur png, jpg, jpeg und gif Dateien!");
+		}
+		
+		//Maximale Bildgröße überprüfen
+		if($_FILES["uploadedfile"]["size"] > $this->max_size)
+		{
+			die ("Bilder können nicht mehr als 2 mb groß sein!");
+		}
+		
+		//Erlaubte Dateiendung
+		$detected_type = exif_imagetype($_FILES["uploadedfile"]["tmp_name"]);
+		
+		if(!in_array($detected_type, $this->allowed_types))
+		{
+			die ("Nur der Upload von Bildern ist erlaubt!");
+		}
+		
+		//Pfad zusammensetzen
+		$new_path = $this->uploadfolder.$filename.".".$extension;
+		
+		//Falls Dateiname bereits vorhanden, Erweiterung des Pfades um nächsthöhere Nummer
+		if(file_exists($new_path))
+		{
+			$id = 1;
+			do 
+			{
+				$new_path = $this->uploadfolder.$filename."_".$id.".".$extension;
+				$id++;
+			} while(file_exists($new_path));
+		
+		return $new_path;
+				
+		}
+	}
+}
