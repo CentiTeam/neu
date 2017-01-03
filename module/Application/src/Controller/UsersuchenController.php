80471c27182d1377e4fc02a0f6fe20789ceed67c
Matthias <Matthias@192.168.178.36>
2017-01-03 15:31:20 +0100

    test

--- module/Application/src/Controller/UsersuchenController.php
+++ module/Application/src/Controller/UsersuchenController.php
@@ -24,8 +24,6 @@ class UsersuchenController extends AbstractActionController
 			$user->suchlisteHolen();
 				
 
-		$liste = User::suchlisteHolen();
-
 		return new ViewModel([
 				'userListe' => $liste,
 		]);
