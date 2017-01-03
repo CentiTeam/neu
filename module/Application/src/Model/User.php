5bb28c13830544bcc999d89c0b0f0878f0d01f0c
Matthias <Matthias@192.168.178.36>
2017-01-03 15:23:46 +0100

    echo

--- module/Application/src/Model/User.php
+++ module/Application/src/Model/User.php
@@ -161,6 +161,7 @@ class User
 	
 		$query="SELECT u_id FROM User WHERE systemadmin = 0 AND username LIKE '".$username."';"; 
 		echo "$username";
+		echo "hallo";
 	
 		// Wenn die Datenbankabfrage erfolgreich ausgeführt worden ist
 		if ($result = $db->execute($query)) {
