c8ab0a9352feccf3a59054c89228fc96a48b2f44
Tanja <Tanja@CoolMaster.fritz.box>
2017-01-03 17:37:57 +0100

    dsGEWG

--- module/Application/src/Controller/GroupoverviewController.php
+++ module/Application/src/Controller/GroupoverviewController.php
@@ -5,7 +5,7 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Application\Model\Gruppe;
-use Application\Model\Gruppenmitglied;
+// use Application\Model\Gruppenmitglied;
 use Application\Model\User;
 
 #TODO nur die Gruppen anzeigen, zu denen man gehört; hierfür "Listeholen aus Gruppenmitglied" und 
@@ -18,33 +18,15 @@ class GroupoverviewController extends AbstractActionController
 		
 		session_start();
 		
-		//Alternative zu den unteren Zeilen zum Abruf der aktuellen g_id
-		//$user = new User();
-		//$user=$_SESSION['user'];
-		//$gruppenliste = Gruppe::eigenelisteHolen($user->getU_id());
-		
 		$user_id=$_SESSION['user']->getU_id();
 		$gruppenliste = Gruppe::eigenelisteholen($user_id);
 		
 		
-		$mitgliederliste = Gruppenmitglied::listeHolen();
-		
-		
-		/** Fehlversuch
-		$mitglieder_ids_array=array();
-		
-		foreach ($mitgliederliste as $gruppenmitglied) {
-			$gruppenmitglied = new Gruppenmitglied();
-			
-			var_dump($gruppenmitglied->getU_id());
-			var_dump($_SESSION['u_id']);
-			
-			if ($gruppenmitglied->getU_id() == $_SESSION['u_id']) {
-				
-				$mitglieder_ids_array[]=$gruppenmitglied->getG_id();
-			}
-		}
-		*/
+
+		//Alternative zu den oberen Zeilen zum Abruf der aktuellen g_id
+		//$user = new User();
+		//$user=$_SESSION['user'];
+		//$gruppenliste = Gruppe::eigenelisteHolen($user->getU_id());
 		
 		/** Alternative, falls "normales" gruppeauflisten spinnt
 		 $anzahl = 0;
@@ -55,9 +37,7 @@ class GroupoverviewController extends AbstractActionController
 		 */
 		
 		return new ViewModel([
-			'gruppenListe' => $gruppenliste,
-			'mitgliederListe' => $mitgliederliste
-			//'mitgliedschaften' => array($mitglieder_ids_array)
+			'gruppenListe' => $gruppenliste
 			// 'anzahl' => $anzahl
 		]);
 		
--- module/Application/src/Controller/GroupshowController.php
+++ module/Application/src/Controller/GroupshowController.php
@@ -10,15 +10,18 @@ class GroupshowController extends AbstractActionController
 {
 	public function GroupshowAction()
 	{
-	
+		
+		// Gruppen-Objekt laden
 		$gruppe= new Gruppe();
-		
 		$g_id=$_REQUEST['g_id'];
-		
 		$gruppe->laden($g_id);
+		
+		// Gruppenmitglieder-Liste holen
+		$mitgliederliste = Gruppe::gruppenmitgliederrlisteholen($g_id);
 		
 		return new ViewModel([
 			'gruppe' => array($gruppe),
+			'mitgliederListe' => $mitgliederliste
 		]);
 		
 	
