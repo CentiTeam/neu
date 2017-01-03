cda4e16dff6a35ffd257095ec9e7e05a6f895b49
Alex <Alex@Alex-PC>
2017-01-03 11:24:46 +0100

    echos entfernen

--- module/Application/src/Controller/BenutzerdeaktivierenController.php
+++ module/Application/src/Controller/BenutzerdeaktivierenController.php
@@ -38,8 +38,7 @@ class BenutzerdeaktivierenController extends AbstractActionController{
 		
 		// wenn das Formular zur Bestätigung des Deaktivierens schon abgesendet wurde, soll dies hier ausgewertet werden
 		if ($_REQUEST['ja']) {
-			echo "test!";
-			echo $isOK;
+
 		
 			$msg = "";
 		
