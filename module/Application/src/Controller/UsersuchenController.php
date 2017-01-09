e5c622841b4b7cad31154a8a66acd696c594783a
Tanja <Tanja@172.19.5.141>
2017-01-09 22:46:32 +0100

    dsac

--- module/Application/src/Controller/UsersuchenController.php
+++ module/Application/src/Controller/UsersuchenController.php
@@ -66,7 +66,7 @@ class UsersuchenController extends AbstractActionController
 				$betreff = "Grouppay: Einladung in die Gruppe $gruppenName";
 				
 				//kann weg
-				//$link="http://132.231.36.206/einladungannehmen?g_id=$g_id?u_id=$empfaenger_id";
+				//$link="http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id";
 				
 
 				$text=
@@ -77,7 +77,7 @@ class UsersuchenController extends AbstractActionController
 						<div>Du wurdest von $absenderVorname $absenderNachname in die Gruppe $gruppenName eingeladen.</div>
 						<br>
 						<div>&Uuml;ber diesen Link kannst Du die Einladung annehmen:</div>
-						<div><a href='http://132.231.36.206/einladungannehmen?g_id=$g_id?u_id=$empfaenger_id'>Einladung annehmen</a></div><br>
+						<div><a href='http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id'>Einladung annehmen</a></div><br>
 						<div>Viele Gr&uuml;&szlig;e</div>
 						<div>Dein Grouppay-Team</div>
 					</body>
