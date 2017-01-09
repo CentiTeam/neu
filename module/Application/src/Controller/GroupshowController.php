e307820b131be0d6b418e92dfee83940df22020f
Alex <Alex@Alex-PC>
2017-01-09 11:23:36 +0100

    h

--- module/Application/src/Controller/GroupshowController.php
+++ module/Application/src/Controller/GroupshowController.php
@@ -15,7 +15,7 @@ class GroupshowController extends AbstractActionController
 	{
 		
 		//Liste alle verfügbaren Kateforien holen
-		$katergorieliste = Kategorie::listeholen();
+		$katergorieliste = Kategorie::listeHolen();
 		
 		// Gruppen-Objekt laden
 		$gruppe= new Gruppe();
