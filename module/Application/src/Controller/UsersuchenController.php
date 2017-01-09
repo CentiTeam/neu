12f226c23f8f28de75b3571c5366700cbba7f585
Tanja <Tanja@172.19.5.141>
2017-01-10 00:38:42 +0100

    eafwa

--- module/Application/src/Controller/UsersuchenController.php
+++ module/Application/src/Controller/UsersuchenController.php
@@ -69,7 +69,7 @@ class UsersuchenController extends AbstractActionController
 				//$link="http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id";
 				////	$link";
 				
-				echo '<a href=\"http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id\">Einladung annehmen</a>';
+				echo "<a href=\"einladungannehmen?g_id=$g_id&u_id=$empfaenger_id\">Einladung annehmen</a>";
 				
 				$text=
 				"<html>
@@ -80,7 +80,7 @@ class UsersuchenController extends AbstractActionController
 						<br>
 						<div>&Uuml;ber diesen Link kannst Du die Einladung annehmen:</div>
 						
-						<a href=\"http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id\">Einladung annehmen</a>
+						<a href=\"http://132.231.36.206/einladungannehmen?g_id=$g_id&u_id=$empfaenger_id\"'>Einladung annehmen</a>
 						<div>Viele Gr&uuml;&szlig;e</div> 
 						<div>Dein Grouppay-Team</div>
 					</body>
