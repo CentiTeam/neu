9714158889638b688a2d7714cbe577b402707080
Tanja <Tanja@172.19.5.141>
2017-01-10 02:02:13 +0100

    faewfe

--- module/Application/src/Controller/EinladungannehmenController.php
+++ module/Application/src/Controller/EinladungannehmenController.php
@@ -59,11 +59,13 @@ class EinladungannehmenController extends AbstractActionController
 			$gruppenmitglied->setGruppenadmin(0);
 				
 			
-			$errStr="";
+			$errStr = "";
+			
 			$gruppenmitgliedListe=Gruppenmitglied::listeholen();
 			
 			foreach ($gruppenmitgliedListe as $liste) {
 				if ($gruppenmitglied->getU_id()==$liste->getU_id() && $gruppenmitglied->getG_id()==$liste->getU_id()) {
+					echo "Hallo";
 					$errStr .= "Du bist bereits Mitglied der Gruppe!";
 				}
 			}
