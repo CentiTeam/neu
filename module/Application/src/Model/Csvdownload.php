174a234f0d91564e28e0a2452db0bf7ce871294d
Alex <Alex@Alex-PC>
2017-01-11 21:06:36 +0100

    j

--- module/Application/src/Model/Csvdownload.php
+++ module/Application/src/Model/Csvdownload.php
@@ -32,13 +32,16 @@ class Csvdownload{
 			$csv .= "\n";
 		}
 		
-		$csv .= '"Column 1 Content","Column 2 Content"' . "\n";
-		$csv .= '"Column 1 Content","Column 2 Content"' . "\n";
-		$csv .= '"Column 1 Content","Column 2 Content"' . "\n";
-		$csv .= '"Column 1 Content","Column 2 Content"' . "\n";
-		$csv .= '"Column 1 Content","Column 2 Content"' . "\n";
-		
 
+		
+		//OUPUT HEADERS
+		header("Pragma: public");
+		header("Expires: 0");
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Cache-Control: private",false);
+		header("Content-Type: application/octet-stream");
+		header("Content-Disposition: attachment; filename=\"$table.csv\";" );
+		header("Content-Transfer-Encoding: binary");
 		
 		//OUTPUT CSV CONTENT
 		//echo($csv);
