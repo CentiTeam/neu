8a078acb698a2f2f9f28f7984469ac5825509283
Alex <Alex@Alex-PC>
2017-01-11 18:44:03 +0100

    CSV Model erstellen

---
+++ module/Application/src/Model/Csvdownload.php
@@ -0,0 +1,25 @@
+<?php
+namespace Application\Model;
+
+class Csvdownload{
+	
+	function makeCsv($file, $dir) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		header("Content-Type: text/csv");
+
+		header("Content-Disposition: attachment; filename=\"$file\"");
+
+		readfile($dir.$file);
+
+	}
+
+}
