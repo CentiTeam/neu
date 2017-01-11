45395e4420ca5df6ea7ed6d6b37197792d70f5a0
Veri <Veri@Verena>
2017-01-11 15:46:53 +0100

    Festgestellt, dass Systemadmin nicht auf Profil zugreifen kann

--- module/Application/src/Controller/ProfilController.php
+++ module/Application/src/Controller/ProfilController.php
@@ -15,7 +15,7 @@ class ProfilController extends AbstractActionController {
 	
 		$errors = array();
 
-		if($_SESSION['angemeldet'] != 'ja') {
+		if($_SESSION['angemeldet'] != 'ja' OR $_SESSION['systemadmin'] != 'ja') {
 				
 			array_push($errors, "Sie müssen angemeldet sein um Ihr Profil zu sehen!");
 				
