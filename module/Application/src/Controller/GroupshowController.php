9a39a50d66e5b65bf49e1c5784441ab096179c48
Matthias <Matthias@192.168.178.36>
2017-01-12 11:52:30 +0100

    d

--- module/Application/src/Controller/GroupshowController.php
+++ module/Application/src/Controller/GroupshowController.php
@@ -55,9 +55,9 @@ class GroupshowController extends AbstractActionController
 				
 			// Werte aus Formular einlesen
 				
-			$text = $_REQUEST ["text"];
+			$text = $_REQUEST ['text'];
 			//$datum = time ();
-			$u_id = $_REQUEST ["u_id"];
+			$u_id = $_REQUEST ['u_id'];
 			$g_id = $_REQUEST ['g_id'];
 
 			
