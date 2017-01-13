e3e9b73e84a95c8004d8e291f02019e60b95d053
Tanja <Tanja@172.19.5.141>
2017-01-13 11:12:36 +0100

    Zalhungsteilnehmer

--- module/Application/src/Model/Zahlungsteilnehmer.php
+++ module/Application/src/Model/Zahlungsteilnehmer.php
@@ -5,26 +5,30 @@ namespace Application\Model;
 use Application\Model\DB_connection;
 
 class Zahlungsteilnehmer {
-	protected  $u_id;
-	protected  $z_id;
+	protected  $user;
+	protected  $zahlung;
 	protected  $status;
 	protected  $anteil;
 	protected  $zahlungsempfaenger; 
 	
-	public function __construct() {
-			
+	// HIER Kann evtl. ein Fehler liegen!!! (Objekt muss beachtet werden?)
+	public function __construct($zahlung_id, $user_id) {
+		$this->zahlung= $zahlung_id;
+		$this->user=$user_id;
+		echo "$this->zahlung";
+		echo "$this->user";
 	}
 	
 	public function anlegen () {
 	
 		$db = new DB_connection();
 	
-		$query = "INSERT INTO zahlungsteilnehmer (u_id, z_id, status, anteil, zahlungsempfaenger) VALUES (
-				'".$this->u_id."',
-				'".$this->z_id."',
+		$query = "INSERT INTO zahlungsteilnehmer (user_id, zahlung_id, status, anteil, zahlungsempfaenger_id) VALUES (
+				'".$this->getUser()->getU_id()."',
+				'".$this->getZahlung()->getZ_id()."',
 				'".$this->status."',
 				'".$this->anteil."',
-				'".$this->zahlungsempfaenger."'
+				'".$this->getZahlungsempfaenger()->getU_id()."'
 				)" ;
 	
 		$result = $db->execute($query);
@@ -32,23 +36,65 @@ class Zahlungsteilnehmer {
 		return $result;
 	}
 	
+	public function laden ($z_id, $u_id) {
+	
+		// Datenbankstatement erzeugen
+		$dbStmt = new DB_connection();
+	
+		// DB-Befehl absetzen: alle Basisinformationen des Teams mit der �bergebenen $t_id abfragen
+	
+		$result=$dbStmt->execute("SELECT * FROM zahlungsteilnehmer 
+				WHERE zahlung_id= '".$z_id."'
+				AND user_id='".$u_id."';");
+		
+		// Variable, die speichert, ob das Team geladen werden konnte oder nicht
+		$isLoaded=false;
+	
+		// Ergebnis verarbeiten, falls vorhanden
+		if ($row=mysqli_fetch_array($result)) {
+			$zahlung_id=$row["zahlung_id"];
+			$this->zahlung=new Zahlung();
+			$this->zahlung->laden($zahlung_id);
+			
+			$user_id=$row["user_id"];
+			$this->user=new User();
+			$this->user->laden($user_id);
+			
+			
+			$this->status=$row["status"];
+			$this->anteil=$row["anteil"];
+			
+			$zahlungsempfänger_id=$row["zahlungsempfänger_id"];
+			$this->zahlungsempfänger=new User();
+			$this->zahlungsempfänger->laden($zahlungsempfänger_id);
+				
+				
+			// speichern, dass die Basisinformationen des Teams erfolgreich geladen werden konnten
+			$isLoaded=true;
+		}
+	
+		// zur�ckgeben, ob beim Laden ein Fehler aufgetreten ist
+		return $isLoaded;
+	}
+	
+	
 	
 	// Getter und Setter
 	
-	public function getU_id () {
-		return $this->u_id;
+	public function getUser () {
+		return $this->user;
 	}
 	
-	public function setU_Id($u_id) {
-		$this->u_id= $u_id;
+	public function setUser($user) {
+		$this->user= $user;
 	}
 	
-	public function getZ_id () {
-		return $this->z_id;
+	public function getZahlung () {
+		return $this->zahlung;
 	}
 		
-	public function setZ_Id($z_id) {
-		$this->z_id= $z_id;
+	public function setZahlung($zahlung) {
+		$this->zahlung= $zahlung;
 	}
 	
 	public function getStatus() {
