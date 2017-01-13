ffabf25136482584abe75515fc1ee74f4637678d
Alex <Alex@Alex-PC>
2017-01-13 11:59:50 +0100

    l

--- module/Application/src/Controller/StatistikenController.php
+++ module/Application/src/Controller/StatistikenController.php
@@ -21,7 +21,7 @@ class statistikenController extends AbstractActionController
 		session_start();
 
 			
- 			$user_id=$_SESSION['user']->getU_id();
+ 			$user_id=$_SESSION['user'];
   			$zahlungenliste = Zahlung::eigenezahlungenholen($user_id);
 			
 			
