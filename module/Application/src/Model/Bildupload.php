416a28ad5f5cc379fef64334737c39b6a20e7205
Veri <Veri@Verena>
2017-01-15 13:56:52 +0100

    Auflösung des Bildes auslesen

--- module/Application/src/Model/Bildupload.php
+++ module/Application/src/Model/Bildupload.php
@@ -31,21 +31,6 @@ class Bildupload
 			return $path;
 		}
 		
-		//Maximale Auflösung überprüfen
-		/** 
-		 $size = getimagesize($uploadedfile);
-		 $width = $size[0];
-		 $height = $size[1];
-		
-		 echo $width;
-		 
-		 if($width < 1024 AND $height < 1024)
-		 {
-		 echo "Die unterst&uumltzte Aufl&oumlsung von Bildern liegt bei 1024*1024";
-		 $path = false;
-		 return $path;
-		 }
-		 **/
 		
 		//Erlaubte Dateiendung
 		$detected_type = exif_imagetype($_FILES["uploadedfile"]["tmp_name"]);
@@ -80,6 +65,21 @@ class Bildupload
 		//Das Bild wird in den Ordner Bilder mit absoluten Pfad verschoben, der relative Pfad wird zurückgegeben
 		if (move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $new_path))
 		{	
+			//Maximale Auflösung überprüfen
+			
+			 $size = getimagesize($new_path);
+			 $width = $size[0];
+			 $height = $size[1];
+			
+			 echo $width;
+			/** 	
+			 if($width < 1024 AND $height < 1024)
+			 {
+			 echo "Die unterst&uumltzte Aufl&oumlsung von Bildern liegt bei 1024*1024";
+			 $path = false;
+			 return $path;
+			 }
+			 **/
 			return $path;
 		}
 		else 
