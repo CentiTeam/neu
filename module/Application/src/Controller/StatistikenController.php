968d4fe7454204fb45335d5975c54db0602dba2e
Aron <Aron@aronzwei>
2017-01-15 17:56:11 +0100

    s

--- module/Application/src/Controller/StatistikenController.php
+++ module/Application/src/Controller/StatistikenController.php
@@ -36,9 +36,9 @@ class StatistikenController extends AbstractActionController
   				if($_REQUEST["afterdate"] != null || $_REQUEST["beforedate"] != null){
   					$zahlungenliste = $this->datFilter($zahlungenliste,$_REQUEST["afterdate"], $_REQUEST["beforedate"]);
   				}
-  				if($_REQUEST["status[]"] != null){
-  					$zahlungenliste = $this->statusFilter($zahlungenliste, $_REQUEST["offen"],$_REQUEST["geschlossen"],$_REQUEST["ersteller"]);
-  				}
+//   				if($_REQUEST["status[]"] != null){
+//   					$zahlungenliste = $this->statusFilter($zahlungenliste, $_REQUEST["offen"],$_REQUEST["geschlossen"],$_REQUEST["ersteller"]);
+//   				}
   				
   			}		
 
@@ -67,13 +67,13 @@ class StatistikenController extends AbstractActionController
  		echo "status Funktion aufgerufen";
  		$filteredlist = array();
  		foreach($zahlungenliste as $zaehler => $zahlungsteilnehmer){
- 			if($zahlungsteilnehmer->getStatus()== 'offen' && $offen == true){
+ 			if($zahlungsteilnehmer->getStatus()== 'offen' && $offen == 'offen'){
  				$filteredlist[] =  $zahlungsteilnehmer;
  			}
- 			if($zahlungsteilnehmer->getStatus()== 'geschlossen' && $geschlossen == true){
+ 			if($zahlungsteilnehmer->getStatus()== 'geschlossen' && $geschlossen == 'geschlossen'){
  				$filteredlist[] =  $zahlungsteilnehmer;
  			}
- 			if($zahlungsteilnehmer->getStatus()== 'ersteller' && $ersteller == true){
+ 			if($zahlungsteilnehmer->getStatus()== 'ersteller' && $ersteller == 'ersteller'){
  				$filteredlist[] =  $zahlungsteilnehmer;
  			}
  		}
