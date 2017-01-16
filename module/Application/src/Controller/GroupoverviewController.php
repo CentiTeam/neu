6e2e5936ddbff137c1db63749f70ecff049f6fdc
Tanja <Tanja@172.19.5.141>
2017-01-16 19:35:44 +0100

    ausmisten

--- module/Application/src/Controller/GroupoverviewController.php
+++ module/Application/src/Controller/GroupoverviewController.php
@@ -15,45 +15,16 @@ class GroupoverviewController extends AbstractActionController
 {
 	public function groupoverviewAction()
 	{
-		
 		session_start();
 		
 		$user_id=$_SESSION['user']->getU_id();
-		//$gruppenliste = Gruppe::eigenelisteholen($user_id);
-		
-		/** Kann raus, wenn Objektorientierung bleibt
-		// Liste der User-Objekte der Gruppenmitglieder holen
-		$mitgliederliste = User::gruppenmitgliederlisteholen($g_id);
-		
-		$gruppenadminListe=array();
-		
-		// F�r jede Gruppe speichern, ob aktueller USer Admin ist und diese Gruppenmitglied-Datensätze
-		// in Array speichern
-		foreach ($gruppenliste as $liste) {
-				
-			// Gruppenmitglied instanzieren
-			$gruppenmitglied= new Gruppenmitglied();
-			$gruppenmitglied->laden ($liste->getG_id(), $user_id);
-				
-			// Wenn Gruppenmitgliedschaft dem User-Objekt entspricht wird das Array weiter bef�llt
-			if ($gruppenmitglied->getGruppenadmin() == true) {
-		
-				$gruppenadminListe[]=$gruppenmitglied;
-		
-			}
-		}
-		*/
 		
 		$gruppenliste=Gruppenmitglied::eigenelisteholen($user_id);
-		
 		
 		
 		return new ViewModel([
 			'gruppenListe' => $gruppenliste,
 			'u_id' => $user_id,
-			// auch ohne Objektorientierung 'gruppenadminListe' => $gruppenadminListe,
 		]);
-		
-	
 	}
 }
