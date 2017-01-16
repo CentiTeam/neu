faaf6fc48246c7264bacf97d4dc665d1eba2ddc2
Matthias <Matthias@192.168.178.36>
2017-01-16 13:23:36 +0100

    d

--- module/Application/src/Model/Nachricht.php
+++ module/Application/src/Model/Nachricht.php
@@ -40,7 +40,43 @@ class Nachricht {
 		return $result;
 	}
 	
+	public function messageboardladen ($n_id = null, $g_id) {
 	
+		// Datenbankstatement erzeugen
+		$dbStmt = new DB_connection();
+	
+		// DB-Befehl absetzen: alle Basisinformationen des Teams mit der �bergebenen $t_id abfragen
+	
+	
+		
+			$result=$dbStmt->execute("SELECT * FROM nachricht WHERE n_id= '".$n_id."'
+										AND g_id = '".$g_id."';");
+		
+		
+		// Variable, die speichert, ob das Team geladen werden konnte oder nicht
+		$isLoaded=false;
+	
+		// Ergebnis verarbeiten, falls vorhanden
+		if ($row=mysqli_fetch_array($result)) {
+			$this->n_id=$row["n_id"];
+			$this->datum=$row["datum"];
+			$this->text=$row["text"];
+	
+			$user_id=$row["u_id"];
+			$this->user=new User();
+			$this->user->laden($user_id);
+	
+			$gruppen_id=$row["g_id"];
+			$this->gruppe=new Gruppe();
+			$this->gruppe->laden($gruppen_id);
+	
+			// speichern, dass die Basisinformationen des Teams erfolgreich geladen werden konnten
+			$isLoaded=true;
+		}
+	
+		// zur�ckgeben, ob beim Laden ein Fehler aufgetreten ist
+		return $isLoaded;
+	}
 	
 	public function laden ($n_id = null) {
 	
@@ -126,7 +162,6 @@ class Nachricht {
 	
 		$query="SELECT * FROM `nachricht`
 				NATURAL JOIN User
-				WHERE nachricht.g_id = '".$g_id."'
 				AND User.u_id= '".$user_id."' 	
 				ORDER BY datum DESC LIMIT 10";
 	
@@ -140,7 +175,7 @@ class Nachricht {
 				$model = new Nachricht();
 	
 				// Model anhand der Nummer aus der Datenbankabfrage laden
-				$model->laden($row["n_id"]);
+				$model->messageboardladen($row["n_id"]);
 	
 				// neues Model ans Ende des $gruppeListe-Arrays anf�gen
 				$aktuelleListe[] = $model;
