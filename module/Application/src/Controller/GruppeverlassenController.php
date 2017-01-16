dab7e390a6f23b89d8c64cb9e1e3107274355ab1
Tanja <Tanja@172.19.5.141>
2017-01-16 22:09:15 +0100

    routing

--- module/Application/src/Controller/GruppeverlassenController.php
+++ module/Application/src/Controller/GruppeverlassenController.php
@@ -6,6 +6,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Application\Model\Gruppe;
 use Application\Model\Gruppenmitglied;
+use Application\Model\User;
 
 #TODO @Tanja Fehler abpr�fen!
 
