bad7881fb9b6b2c91d3c1d99f1770d4bc125db5c
Matthias <Matthias@192.168.178.36>
2017-01-17 21:42:41 +0100

    controller fertig

--- module/Application/src/Controller/PasswordresetController.php
+++ module/Application/src/Controller/PasswordresetController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class PasswordresetController extends AbstractActionController
+{
+	public function passwordresetAction()
+	{
+
+
+		return new ViewModel();
+
+	}
