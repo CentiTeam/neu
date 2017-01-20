3e69ac5ded5e0a9c7e1f781c4e36ce58c2a16f49
Tanja <Tanja@172.19.5.141>
2017-01-20 20:03:21 +0100

    PRfoilbild in Gruppe als Link

---
+++ module/Application/src/Controller/FremdesprofilController.php
@@ -0,0 +1,89 @@
+<?php
+namespace Application\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Application\Model\User;
+use Application\Model\Bildupload;
+
+class ProfilController extends AbstractActionController {
+
+	function profilAction() {
+		// TODO Berechtigungspr�fung
+		session_start();
+
+		$errors = array();
+
+		if($_SESSION['angemeldet'] == 'ja') {
+				
+			$u_id=$_REQUEST['u_id'];
+			$user = new User();
+			$user->laden($u_id);
+
+			$saved= false;
+			$msg = array();
+
+			if ($_REQUEST['profilbild']) {
+
+				$bildupload = new Bildupload();
+
+				// Schritt 1:  Werte aus Formular einlesen
+				$uploadedfile=$_REQUEST["uploadedfile"];
+
+				//Bilddatei an die Funktion Bildupload �bergeben, R�ckgabe des Bildpfades
+				$path = $bildupload->bildupload($uploadedfile);
+
+				$u_id=$_REQUEST["u_id"];
+
+				if ($path!=false) {
+
+					$result = User::bild($path, $u_id);
+						
+					$user->laden($u_id);
+
+					return new ViewModel([
+							'user' => array($user),
+					]);
+
+					$view->setTemplate('application/profil/profil.phtml');
+						
+					return $view;
+				}
+
+				else {
+						
+					$user = new User();
+						
+					$user->laden($u_id);
+						
+					$view = new ViewModel([
+							'user' => array($user)
+					]);
+						
+					$view->setTemplate('application/profil/profil.phtml');
+
+					return $view;
+				}
+
+			}
+
+			return new ViewModel([
+					'user' => array($user),
+			]);
+
+
+		} else {
+				
+			array_push($errors, "Sie müssen angemeldet sein um Ihr Profil zu sehen!");
+
+			$view = new ViewModel(array(
+					$errors
+			));
+			$view->setTemplate('application/index/index.phtml');
+			return $view;
+				
+		}
+	}
+
+}
