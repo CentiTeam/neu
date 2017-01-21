b592e28a6533d9968f6dfffffde6e5af04e64b4e
Tanja <Tanja@172.19.5.141>
2017-01-21 12:48:43 +0100

    dafe

--- module/Application/src/Controller/GroupeditController.php
+++ module/Application/src/Controller/GroupeditController.php
@@ -145,7 +145,7 @@ class GroupeditController extends AbstractActionController {
 				 
 				 // Gruppenmitgliedsdaten des aktuellen Nutzer laden wg. Adminprüfung
 				 $aktgruppenmitglied=$gruppenmitglied->laden($g_id, $user_id);
-				 
+				 var_dump($aktgruppenmitglied);
 				 // Gruppenmitgliederliste anzeigen
 				 $mitgliederliste=Gruppenmitglied::gruppenmitgliederlisteHolen($g_id);
 				 
