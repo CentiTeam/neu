90c0156eb7b682dc908a9f2e785dc6081fb32c5d
Aron <Aron@aronzwei>
2017-01-21 09:59:33 +0100

    hm

--- module/Application/src/Model/Zahlungsteilnehmer.php
+++ module/Application/src/Model/Zahlungsteilnehmer.php
@@ -98,10 +98,20 @@ class Zahlungsteilnehmer {
  				//Schuldstand verändert sich nur 
  				//falls user selber Ersteller/Empfänger ist
  				
- 				if($einzahlungsteilnehmer->getZahlungsempfaenger()== $einzahlungsteilnehmer->getUser() ->getU_id()){
- 					$schuldstand += $andererzahlungsteilnehmer ->getRestbetrag();
+//  				$alleteilnehmerausgeminsamerzahlung = zahlungsteilnehmerholen($zahlungsteilnehmer -> getZahlung() -> getZ_id());
+//  				foreach($alleteilnehmerausgeminsamerzahlung as $zaehler => $zufilternderteilnehmer)
+ 				
+ 					
+ 					$ersterzahlungsteilnehmer = einenzahlungsteilnehmerholen($zahlungsteilnehmer -> getZahlung() -> getZ_id(), 
+ 							$einzahlungsteilnehmer->getUser() ->getU_id());
+ 					
+ 					$zweiterzahlungsteilnehmer = einenzahlungsteilnehmerholen($zahlungsteilnehmer -> getZahlung() -> getZ_id(),
+ 							$andererzahlungsteilnehmer->getUser() ->getU_id());
+ 					
+ 				if($ersterzahlungsteilnehmer->getZahlungsempfaenger()== $ersterzahlungsteilnehmer->getUser() ->getU_id()){
+ 					$schuldstand += $zweiterzahlungsteilnehmer ->getRestbetrag();
  				}else{
- 					$schuldstand -= $einzahlungsteilnehmer ->getRestbetrag();
+ 					$schuldstand -= $ersterzahlungsteilnehmer ->getRestbetrag();
  				}
  				
  			}
@@ -233,6 +243,38 @@ class Zahlungsteilnehmer {
 		
 			// fertige Liste von Gruppe-Objekten zurï¿½ckgeben
 			return $teilnehmerListe;
+		}
+	}
+	
+	
+	public function einenzahlungsteilnehmerholen($z_id, $u_id){
+		// Liste initialisieren
+		$zteilnehmer;
+	
+		$db = new DB_connection();
+	
+		$query="SELECT * FROM `zahlungsteilnehmer`
+				WHERE z_id= '".$z_id."' 
+				AND WHERE u_id= '".$u_id."' ";
+	
+		// Wenn die Datenbankabfrage erfolgreich ausgefï¿½hrt worden ist
+		if ($result = $db->execute($query)) {
+	
+			// Ergebnis Zeile fï¿½r Zeile verarbeiten
+			while ($row = mysqli_fetch_array($result)) {
+					
+				// neues Model erzeugen
+				$model = new Zahlungsteilnehmer();
+	
+				// Model anhand der Nummer aus der Datenbankabfrage laden
+				$model->laden($row["z_id"], $row["u_id"]);
+	
+				// neues Model ans Ende des $gruppeListe-Arrays anfï¿½gen
+				$zteilnehmer = $model;
+			}
+	
+			// fertige Liste von Gruppe-Objekten zurï¿½ckgeben
+			return $zteilnehmer;
 		}
 	}
 	
