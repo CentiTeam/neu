e24171de370b192388559b1c08ea828d5ef64cb8
Veri <Veri@Verena>
2017-01-21 16:03:58 +0100

    Ups

--- module/Application/src/Controller/NachrichtbearbeitenController.php
+++ module/Application/src/Controller/NachrichtbearbeitenController.php
@@ -4,7 +4,7 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Zend\View\Model\Nachricht;
+use Application\Model\Nachricht;
 
 class NachrichtbearbeitenController extends AbstractActionController {
 
