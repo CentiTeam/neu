df2d7c9f14a1d7f91a309a767882654b52b60028
Veri <Veri@Verena>
2017-01-21 16:18:42 +0100

    Les des blöde Zeug aus

--- module/Application/src/Controller/NachrichtbearbeitenController.php
+++ module/Application/src/Controller/NachrichtbearbeitenController.php
@@ -55,7 +55,7 @@ class NachrichtbearbeitenController extends AbstractActionController {
 			}
 			
 			return new ViewModel([
-					'nachricht' => $nachricht,
+					'nachricht' => array($nachricht),
 			]);
 			
 		}
