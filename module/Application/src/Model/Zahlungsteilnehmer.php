3881d87eec0646420c5ff1dd0ee9a41376e3f700
Aron <Aron@aronzwei>
2017-01-21 14:39:24 +0100

    t

--- module/Application/src/Model/Zahlungsteilnehmer.php
+++ module/Application/src/Model/Zahlungsteilnehmer.php
@@ -134,7 +134,7 @@ class Zahlungsteilnehmer {
  							
  							$zweiterzahlungsteilnehmer = $zahlungsteilnehmer-> einenzahlungsteilnehmerholen($zahlungsteilnehmer -> getZahlung() -> getZ_id(),
  									$andererzahlungsteilnehmer->getUser() ->getU_id());
- 							if($einzahlungsteilnehmer ->getUser() ->getU_id()== $zweiterzahlungsteilnehmer->getZahlungsempfaenger()->getU_id()
+ 							if($ersterzahlungsteilnehmer ->getUser() ->getU_id()== $ersterzahlungsteilnehmer->getZahlungsempfaenger()->getU_id()
  									&&($zweiterzahlungsteilnehmer ->getRestbetrag()) >0){
  								//
  								// 
