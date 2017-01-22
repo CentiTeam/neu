989aee5107d8ddbaa589b1e6acd828c8481473cb
Alex <Alex@Alex-PC>
2017-01-22 20:42:24 +0100

    l

--- module/Application/src/Controller/GruppeverlassenController.php
+++ module/Application/src/Controller/GruppeverlassenController.php
@@ -96,7 +96,7 @@ class GruppeverlassenController extends AbstractActionController
 				$msg .= "Gruppenmitgliedschaft erfolgreich gel&ouml;scht!<br>";
 				
 				//Schreiben des Gruppenaustritts in die Ereignistabelle der Datenbank
-				Gruppenereignis::gruppenmitgliedaustretenEreignis($user, $gruppe);
+				Gruppenereignis::gruppenmitgliedaustretenEreignis($user, $gruppenmitglied->getGruppe()->getG_id()); 
 
 			} else {
 
