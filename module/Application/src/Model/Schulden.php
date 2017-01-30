3c0d354cf82dc1c9a6eb165f40a8c8fc3afacd8b
Alexander <Alexander@Uni-Laptop>
2017-01-30 20:22:29 +0100

    feierabend

--- module/Application/src/Model/Schulden.php
+++ module/Application/src/Model/Schulden.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Application\Model;
+
+
+use Application\Model\DB_connection;
+
+
+class Schulden{
+	protected $betragvonglaeubigeranschuldner;
+	protected $betragvonschuldneranglaeubiger;
+	protected $glaeubiger;
+	protected $schuldner;
+	
+}
