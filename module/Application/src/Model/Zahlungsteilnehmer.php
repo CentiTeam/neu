170a1fbd953c11b7447e1f40241407d09e201021
Aron <Aron@aronzwei>
2017-01-31 16:50:21 +0100

    t

--- module/Application/src/Model/Zahlungsteilnehmer.php
+++ module/Application/src/Model/Zahlungsteilnehmer.php
@@ -61,31 +61,36 @@ class Zahlungsteilnehmer {
 			$restbetrag=0;
 			$status="beglichen";
 			$uebrig=($betrag - $temp);
-		}else{
+		}elseif($betrag>=0){
 			$restbetrag=($zahlungsteilnehmer->getRestbetrag()-$betrag);
 			//$status=$this->getStatus;
 			$status = "offen";
 			$uebrig=0;
 		}
-	$db = new DB_connection();	
+		restbetragQuery($restbetrag, $status, $zahlungsteilnehmer);
 	
-	$query =	"UPDATE zahlungsteilnehmer SET restbetrag = '".$restbetrag."', status = '".$status."'
-				WHERE z_id='".$zahlungsteilnehmer->getZahlung()->getZ_id()."' 
-				AND u_id='".$zahlungsteilnehmer->getUser()->getU_id()."';";
-	$result = $db->execute($query);
 	return $uebrig;
 	
 	}
+	public function restbetragQuery($restbetrag, $status, $zahlungsteilnehmer){
+		$db = new DB_connection();
+		
+		$query =	"UPDATE zahlungsteilnehmer SET restbetrag = '".$restbetrag."', status = '".$status."'
+				WHERE z_id='".$zahlungsteilnehmer->getZahlung()->getZ_id()."'
+				AND u_id='".$zahlungsteilnehmer->getUser()->getU_id()."';";
+		$result = $db->execute($query);
+		
+	}
 	
-	public function alleausgleichen($z_id){
-		$teilnehmerListe = $this->zahlungsteilnehmerholen($this->getZahlung()->getZ_id());
-		foreach($teilnehmerListe as $zaehler => $zahlungsteilnehmer){
-			$zahlungsteilnehmer -> ausgleichen($zahlungsteilnehmer);
-		}
+// 	public function alleausgleichen($z_id){
+// 		$teilnehmerListe = $this->zahlungsteilnehmerholen($this->getZahlung()->getZ_id());
+// 		foreach($teilnehmerListe as $zaehler => $zahlungsteilnehmer){
+// 			$zahlungsteilnehmer -> ausgleichen($zahlungsteilnehmer);
+// 		}
 		//eigentlich muss nur jeder einzelne User mit dem Ersteller verglichen werden
 		//die Funktion ausgleichen() wird f�r jeden User, der mit der erstellten(oder bearbeiteten)
 		//Zahlung etwas zu tuen hat von dieser Funktion aus aufgerufen
-	}
+	//}
 	
 	
 	public function ausgleichen() {
@@ -152,6 +157,7 @@ class Zahlungsteilnehmer {
  								// 
  									
  									$schuldstand = $zweiterzahlungsteilnehmer -> zahlungbegleichen($schuldstand, $zweiterzahlungsteilnehmer);
+ 									$this->
  									}
  						}
  					}
