ae617f62de1fdbacf9597d6e796ed5dd397e1c3b
Mattaaal <buchbauer.matthias@yahoo.de>
2017-02-01 21:06:51 +0100

    sd

--- module/Application/src/Controller/GroupshowController.php
+++ module/Application/src/Controller/GroupshowController.php
@@ -35,7 +35,7 @@ class GroupshowController extends AbstractActionController
 		
 		}
 		
-		$g_id=$_GET['g_id'];
+		$g_id=$_REQUEST['g_id'];
 		$user_id=$_SESSION['user']->getU_id();
 		
 		$aktgruppenmitglied=new Gruppenmitglied();
