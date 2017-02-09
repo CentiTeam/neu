032413dd2b848a8cf81d7e99a5257e5fc38b27dc
Alexander <Alexander@Uni-Laptop>
2017-02-09 20:02:42 +0100

    monat

--- module/Application/src/Controller/ZahlunganlegenController.php
+++ module/Application/src/Controller/ZahlunganlegenController.php
@@ -130,7 +130,9 @@ class ZahlunganlegenController extends AbstractActionController {
 						$mm = substr($zahlungsdatum, 5, 1);
 						//Testen, ob Monat richtig eingegeben wurde als Zahl zwischen 1 und 12
 						if(ctype_digit($mm) == true){
-							if($mm>0 && $mm<13){
+							//Konvertieren des Strings in einen Integer
+							$mm_int = intval($mm);
+							if($mm_int>0 && $mm_int<13){
 								$bool_monat_okay = true;
 								echo '<meta http-equiv="refresh" content="1; URL=http://www.example.com/';
 							}
